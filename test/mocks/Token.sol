7f1a6e4a79483700ef6e943166b1082216655859
decanus <dean.eigenmann@icloud.com>
2018-01-01 17:14:44 +0100

    added token mock

---
+++ test/mocks/Token.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.4.18;
+
+contract Token {
+
+    mapping (address => uint) balances;
+
+    function balanceOf(address owner) public view returns (uint) {
+        return balances[owner];
+    }
+
+    function transfer(address to, uint value) public returns (bool) {
+        balances[msg.sender] = balances[msg.sender] - value;
+        balances[to] = balances[to] + value;
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint value) public returns (bool) {
+        balances[from] = balances[from] - value;
+        balances[to] = balances[to] + value;
+        return true;
+    }
+
+    function mint(address to, uint _amount) public {
+        balances[to] = balances[to] + _amount;
+    }
+}
