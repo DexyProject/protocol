3ecbe7ae57d9a117f673535b6c7ec786b9b7c462
Dean Eigenmann <dean@tokenate.io>
2018-01-04 22:57:48 +0100

    enhancement/events-cleanup (#1)

--- contracts/ExchangeInterface.sol
+++ contracts/ExchangeInterface.sol
@@ -2,6 +2,11 @@ pragma solidity ^0.4.18;
 
 interface ExchangeInterface {
 
+    event Deposited(address indexed user, address token, uint amount);
+    event Withdrawn(address indexed user, address token, uint amount);
+    event Cancelled(bytes32 indexed hash);
+    event Traded();
+
     function deposit(address token, uint amount) external payable;
     function withdraw(address token, uint amount) external;
     function balanceOf(address token, address user) public view returns (uint);
