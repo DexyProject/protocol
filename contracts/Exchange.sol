e648f0a7146ce1bf3b9ca46cf8ad0683b2a4e904
mattdf <work@mattdf.com>
2018-01-05 01:41:04 +0000

    add trade functionality and some management functions

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -11,8 +11,14 @@ contract Exchange is Ownable, ExchangeInterface {
 
     address constant ETH = 0x0;
 
-    /// (token => (user => balance))
+	/// exchange parameters
+	uint makerFee = 0;
+	uint takerFee = 0;
+	address feeAccount;
+
+    /// state maps
     mapping (address => mapping (address => uint)) balances;
+	mapping (address => mapping (bytes32 => uint)) fills;
     mapping (bytes32 => bool) cancelled;
 
     function Exchange() public { }
@@ -48,8 +54,19 @@ contract Exchange is Ownable, ExchangeInterface {
         Withdrawn(msg.sender, token, amount);
     }
 
+	function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) public {
+
+		bytes32 hash = sha256(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, user, this);
+
+		if (ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash), v, r, s) != user) revert();
+
+		performTrade(tokenGet, amountGet, tokenGive, amountGive, user, amount);
+		fills[user][hash] = fills[user][hash].add(amount);
+		Traded();
+	}
+
     function cancel(uint expires, uint amountGive, uint amountGet, address tokenGet, address tokenGive, uint nonce, uint8 v, bytes32 r, bytes32 s) external {
-        bytes32 hash = keccak256(msg.sender, expires, amountGive, amountGet, tokenGet, tokenGive, nonce);
+		bytes32 hash = sha256(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, msg.sender, this);
         require(didSign(msg.sender, hash, v, r, s));
 
         cancelled[hash] = true;
@@ -63,6 +80,29 @@ contract Exchange is Ownable, ExchangeInterface {
     function canTrade() public view returns (bool) {
         return false;
     }
+
+	function performTrade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address user, uint amount) internal {
+
+		uint tradeTakerFee = amount.mul(takerFee).div(1 ether);
+		uint tradeMakerFee = amount.mul(makerFee).div(1 ether);
+
+
+		balances[tokenGet][msg.sender] = balances[tokenGet][msg.sender].sub(amount.add(tradeTakerFee));
+		balances[tokenGet][user] = balances[tokenGet][user].add(amount.sub(tradeMakerFee));
+		balances[tokenGet][feeAccount] = balances[tokenGet][feeAccount].add(amount.add(tradeTakerFee).add(tradeMakerFee));
+		balances[tokenGive][user] = balances[tokenGive][user].sub(amountGive.mul(amount).div(amountGet));
+		balances[tokenGive][msg.sender] = balances[tokenGive][msg.sender].add(amountGive.mul(amount).div(amountGet));
+
+	}
+
+	function setFees(uint _makerFee, uint _takerFee) onlyOwner public {
+		makerFee = _makerFee;
+		takerFee = _takerFee;
+	}
+
+	function setFeeAccount(address _feeAccount) onlyOwner public {
+		feeAccount = _feeAccount;
+	}
 
     function didSign(address addr, bytes32 hash, uint8 v, bytes32 r, bytes32 s) internal pure returns (bool) {
         return ecrecover(keccak256("\x19Ethereum Signed Message:\n32", hash), v, r, s) == addr;
