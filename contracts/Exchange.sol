3a74b898e270713b2d2d791c25360043c213dad6
mattdf <work@mattdf.com>
2018-01-05 17:36:57 +0000

    prevent wash trading

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -53,6 +53,7 @@ contract Exchange is Ownable, ExchangeInterface {
     }
 
     function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) external {
+		if (msg.sender == user) revert();
         bytes32 hash = keccak256(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, user, this);
         require(canTrade(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, user, v, r, s, amount, hash));
 
