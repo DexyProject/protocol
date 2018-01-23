9219644946a92ebab505c4c262254a4879b5a3ae
decanus <dean.eigenmann@icloud.com>
2018-01-23 15:41:24 +0100

    minor fix

--- contracts/Vault/VaultInterface.sol
+++ contracts/Vault/VaultInterface.sol
@@ -2,4 +2,9 @@ pragma solidity ^0.4.18;
 
 interface VaultInterface {
 
+    function deposit(address token, uint amount) external payable;
+    function withdraw(address token, uint amount) external;
+    function transfer(address token, address from, address to, uint amount) external;
+    function balanceOf(address token, address user) public view returns (uint);
+
 }
