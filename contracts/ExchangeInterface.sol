38d52f5e5d97ec7ef948400f9206db4b6911e59b
Dean Eigenmann <dean@tokenate.io>
2018-02-06 15:09:29 +0100

    enhancement/signer-types (#12)
    * updated to use mode
    
    * Fixed interface, stack too deep
    
    * stack depth
    
    * Rewrote

--- contracts/ExchangeInterface.sol
+++ contracts/ExchangeInterface.sol
@@ -9,9 +9,11 @@ interface ExchangeInterface {
 
     function deposit(address token, uint amount) external payable;
     function withdraw(address token, uint amount) external;
-    function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) external;
-    function cancel(uint expires, uint amountGive, uint amountGet, address tokenGet, address tokenGive, uint nonce, uint8 v, bytes32 r, bytes32 s) external;
+    function trade(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint amount, uint mode) external;
+    function cancel(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint mode) external;
     function balanceOf(address token, address user) public view returns (uint);
-    function canTrade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount, bytes32 hash) public view returns (bool);
+    function filled(address user, bytes32 hash) public view returns (uint);
+    function canTrade(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint amount, uint mode) public view returns (bool);
+    function getVolume(uint amountGet, address tokenGive, uint amountGive, address user, bytes32 hash) public view returns (uint);
 
 }
