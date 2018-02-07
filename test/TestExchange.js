397186f413e9e8d8d1b77f582b55b3397ea0a585
decanus <dean.eigenmann@icloud.com>
2018-02-07 23:06:11 +0100

    updates

--- test/TestExchange.js
+++ test/TestExchange.js
@@ -13,15 +13,40 @@ contract('Organization', function (accounts) {
     context('funds', async () => {
 
         it('should allow depositing of token', async () => {
-            await token.mint(accounts[0], 30);
-            await exchange.deposit(token.address, 15, {from: accounts[0]});
-            assert.equal(await exchange.balanceOf.call(token.address, accounts[0]), 15);
+            let total = 30;
+            let using = total / 2;
+
+            await token.mint(accounts[0], total);
+            await exchange.deposit(token.address, using, {from: accounts[0]});
+            assert.equal(await exchange.balanceOf.call(token.address, accounts[0]), using);
         });
 
         it('should allow depositing of ether', async () => {
-            await exchange.deposit(0x0, 15, {from: accounts[0], value: 10});
+            await exchange.deposit(0x0, 0, {from: accounts[0], value: 10});
             assert.equal(await exchange.balanceOf.call(0x0, accounts[0]), 10);
         });
 
+        it('should allow withdrawing of tokens', async () => {
+            let total = 30;
+            let using = total / 2;
+
+            await token.mint(accounts[0], total);
+            await exchange.deposit(token.address, using, {from: accounts[0]});
+            assert.equal(await exchange.balanceOf.call(token.address, accounts[0]), using);
+
+            await exchange.withdraw(token.address, using, {from: accounts[0]});
+            assert.equal(await exchange.balanceOf.call(token.address, accounts[0]), 0);
+            assert.equal(await token.balanceOf.call(accounts[0]), total);
+        });
+
+        it('should allow withdrawing of ether', async () => {
+            let using = 15;
+
+            await exchange.deposit(0x0, using, {from: accounts[0], value: using});
+            assert.equal(await exchange.balanceOf.call(0x0, accounts[0]), using);
+
+            await exchange.withdraw(0x0, using, {from: accounts[0]});
+            assert.equal(await exchange.balanceOf.call(0x0, accounts[0]), 0);
+        });
     });
 });
