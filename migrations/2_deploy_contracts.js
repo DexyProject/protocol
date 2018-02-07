2103d2d6e04f9bef89bf9fe222029a2755ea9e23
decanus <dean.eigenmann@icloud.com>
2018-02-07 22:41:50 +0100

    updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,1 +1,9 @@
-module.exports = async (deployer) => { };
+const SafeMath = artifacts.require("./SafeMath.sol");
+const Exchange = artifacts.require("./Exchange.sol");
+
+module.exports = async (deployer) => {
+
+    await deployer.deploy(SafeMath);
+    await deployer.deploy(Exchange);
+    await deployer.link(SafeMath, [Exchange]);
+};
