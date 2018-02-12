7fbbe06eb3fdc69712bd54547e1645b81de6597d
decanus <dean.eigenmann@icloud.com>
2018-02-12 08:20:47 +0100

    migration fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,9 @@
 const SafeMath = artifacts.require("./SafeMath.sol");
 const Exchange = artifacts.require("./Exchange.sol");
 
-module.exports = async (deployer) => {
+module.exports = async (deployer, network, accounts) => {
 
     await deployer.deploy(SafeMath);
-    await deployer.deploy(Exchange);
+    await deployer.deploy(Exchange, 0, accounts[0]);
     await deployer.link(SafeMath, [Exchange]);
 };
