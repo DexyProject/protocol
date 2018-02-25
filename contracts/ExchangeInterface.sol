113664e7464904dd2d0d4ab6cce8add243f157f7
decanus <dean.eigenmann@icloud.com>
2018-02-25 11:44:18 +0100

    fixes

--- contracts/ExchangeInterface.sol
+++ contracts/ExchangeInterface.sol
@@ -5,8 +5,6 @@ interface ExchangeInterface {
     event Cancelled(bytes32 indexed hash);
     event Traded(bytes32 indexed hash, address tokenGive, uint amountGive, address tokenGet, uint amountGet, address maker, address taker);
 
-    function deposit(address token, uint amount) external payable;
-    function withdraw(address token, uint amount) external;
     function trade(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint amount, uint mode) external;
     function cancel(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint mode) external;
     function filled(address user, bytes32 hash) public view returns (uint);
