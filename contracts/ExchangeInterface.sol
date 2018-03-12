4a0197fe327a002b45b3081241aaac86f1abcbff
Dean Eigenmann <dean@tokenate.io>
2018-03-12 11:14:37 +0100

    Update ExchangeInterface.sol

--- contracts/ExchangeInterface.sol
+++ contracts/ExchangeInterface.sol
@@ -6,7 +6,7 @@ interface ExchangeInterface {
     event Traded(bytes32 indexed hash, address tokenGive, uint amountGive, address tokenGet, uint amountGet, address maker, address taker);
 
     function trade(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint amount, uint mode) external;
-    function cancel(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint mode) external;
+    function cancel(address[3] addresses, uint[4] values) external;
     function filled(address user, bytes32 hash) public view returns (uint);
     function canTrade(address[3] addresses, uint[4] values, uint8 v, bytes32 r, bytes32 s, uint amount, uint mode) public view returns (bool);
     function getVolume(uint amountGet, address tokenGive, uint amountGive, address user, bytes32 hash) public view returns (uint);
