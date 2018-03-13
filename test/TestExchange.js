21c7c9c78fb9cf54ebb353cad1e4ba9556f8aca2
decanus <dean.eigenmann@icloud.com>
2018-03-13 20:42:49 +0100

    Added cancellation tests

--- test/TestExchange.js
+++ test/TestExchange.js
@@ -4,6 +4,8 @@ const utils = require('./helpers/Utils.js');
 
 contract('Exchange', function (accounts) {
 
+    const schema_hash = '0xa8da5e6ea8c46a0516b3a2e3b010f264e8334214f4b37ff5f2bc8a2dd3f32be1';
+
     let vault, exchange;
 
     beforeEach(async () => {
@@ -19,6 +21,44 @@ contract('Exchange', function (accounts) {
         }
 
         assert.fail('depositing ether did not fail');
+    });
+
+    describe('cancel', async () => {
+
+        let order, addresses, values;
+
+        beforeEach(async () => {
+            order = {
+                tokenGet: '0xc5427f201fcbc3f7ee175c22e0096078c6f584c4',
+                amountGet: '10',
+                tokenGive: '0x000000000000000000000000000000000000000',
+                amountGive: '100',
+                expires: Math.floor((Date.now() / 1000) + 5000),
+                nonce: 10,
+                user: accounts[0],
+                exchange: exchange.address
+            };
+
+            addresses = [order.user, order.tokenGive, order.tokenGet];
+            values = [order.amountGive, order.amountGet, order.expires, order.nonce];
+        });
+
+
+        it('should allow user to cancel own order', async () => {
+            let result = await exchange.cancel(addresses, values);
+            assert.equal(result['logs'][0]['event'], 'Cancelled');
+        });
+
+
+        it('should prevent user to cancel other users order', async () => {
+            try {
+                await exchange.cancel(addresses, values, {from: accounts[1]});
+            } catch (error) {
+                return utils.ensureException(error);
+            }
+
+            assert.fail('cancelling did not fail');
+        });
     });
 
 });
