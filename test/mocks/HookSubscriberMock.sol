5cc749b1d29cc0a0f8933cc8d83153e57c6a4441
decanus <dean.eigenmann@icloud.com>
2018-04-23 20:16:16 +0200

    extended mock

--- test/mocks/HookSubscriberMock.sol
+++ test/mocks/HookSubscriberMock.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.4.18;
 
+import "./../../contracts/ExchangeInterface.sol";
+
 contract HookSubscriberMock {
 
     mapping (address => uint) public tokens;
@@ -8,5 +10,10 @@ contract HookSubscriberMock {
         tokens[token] += amount;
     }
 
-    // @todo create order etc.
+    function createOrder(address token, uint amount, ExchangeInterface exchange) external {
+        exchange.order(
+            [token, address(0x0)],
+            [amount, 10, (now + 10 days), now]
+        );
+    }
 }
